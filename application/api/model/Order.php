a2650bb972072826d94e52aca18300ceb7774783
AilonHuang <529678737@qq.com>
2018-02-24 23:37:34 +0800

    订单创建

---
+++ application/api/model/Order.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yilun
+ * Date: 2018/2/24
+ * Time: 22:24
+ */
+
+namespace app\api\model;
+
+
+class Order
+{
+
+}
---
+++ application/api/model/OrderProduct.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yilun
+ * Date: 2018/2/24
+ * Time: 22:40
+ */
+
+namespace app\api\model;
+
+
+class OrderProduct
+{
+
+}
