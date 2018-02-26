1ac96d9d93700feb6f5b77c7f9a486b7e0253b1f
AilonHuang <529678737@qq.com>
2018-02-26 23:02:16 +0800

    历史订单, 订单详情

--- application/api/model/Order.php
+++ application/api/model/Order.php
@@ -13,4 +13,28 @@ class Order extends BaseModel
 {
     protected $hidden = ['user_id', 'delete_time', 'update_time'];
     protected $autoWriteTimestamp = true;
+
+    public function getSnapItemsAttr($value)
+    {
+        if(empty($value)) {
+            return null;
+        }
+        return json_decode($value);
+    }
+
+    public function getSnapAddressAttr($value)
+    {
+        if(empty($value)) {
+            return null;
+        }
+        return json_decode($value);
+    }
+
+    public static function getSummaryBuUser($uid, $page = 1, $size = 15)
+    {
+        $pagingData = self::where('user_id', '=', $uid)
+            ->order('create_time', 'desc')
+            ->paginate($size, false, ['page' => $page]);
+        return$pagingData;
+    }
 }
